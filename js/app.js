5e5bb99557293e88e319da55a902025c2de8b313
AkromCik <@akromjonrahimjon712>
2022-07-19 19:24:54 +0500

    add edit function and delete function

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ const minuteEl = document.getElementById("minute");
 const secondEl = document.getElementById("second");
 const closeEl = document.getElementById("close");
 
+let editIdItem;
+
 let result = JSON.parse(localStorage.getItem("result"))
   ? JSON.parse(localStorage.getItem("result"))
   : [];
@@ -35,7 +37,7 @@ function getTime() {
     now.getHours() < 10 ? "0" + (now.getHours() + 1) : now.getHours();
   const minutes =
     now.getMinutes() < 10 ? "0" + (now.getMinutes() + 1) : now.getMinutes();
-    const seconds =
+  const seconds =
     now.getSeconds() < 10 ? "0" + (now.getSeconds() + 1) : now.getSeconds();
   let months = [
     "January",
@@ -52,19 +54,19 @@ function getTime() {
     "December",
   ];
 
-  let monthTitle = now.getMonth()
+  let monthTitle = now.getMonth();
 
   fullDay.textContent = `${date} ${months[monthTitle]}, ${year}`;
 
-  hourEl.textContent = hour
-  minuteEl.textContent = minutes
-  secondEl.textContent = seconds
+  hourEl.textContent = hour;
+  minuteEl.textContent = minutes;
+  secondEl.textContent = seconds;
   return `${hour}:${minutes}, ${date}.${month}.${year}`;
 }
 
 setInterval(() => {
-  getTime()
-},1000)
+  getTime();
+}, 1000);
 
 // Show todos any appendToDom
 
@@ -73,12 +75,16 @@ function appendToDom() {
   listGroupTodo.innerHTML = "";
   result.forEach((el, i) => {
     listGroupTodo.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between ">
-        ${el.name}
+    <li ondblclick="setComplated(${i})" class="list-group-item d-flex justify-content-between ${`${
+      el.isComplate == true ? "complated" : ""
+    }`}">
+        
+    ${el.name}
+    
     <div class="todo-icons">
       <span class="opacity-50 me-3">${el.time}</span>
-      <img src="./img/edit.svg" alt="edit as input" width="25" height="25">
-      <img src="./img/delete.svg" alt="edit as input" width="25" height="25">
+      <img onclick = (editTodo(${i})) src="./img/edit.svg" alt="edit as input" width="25" height="25">
+      <img onclick=(deleteTodo(${i})) src="./img/delete.svg" alt="edit as input" width="25" height="25">
     </div>
     </li>
     `;
@@ -89,7 +95,7 @@ function appendToDom() {
 // <div class="todo-icons">
 //   <span class="opacity-50 me-3">18.07.2022</span>
 //   <img src="./img/edit.svg" alt="edit as input" width="25" height="25">
-//   <img src="./img/delete.svg" alt="edit as input" width="25" height="25">
+//   <img onclick() src="./img/delete.svg" alt="delete as input" width="25" height="25">
 // </div>
 // </li>
 
@@ -118,3 +124,75 @@ formCreate.addEventListener("submit", (evt) => {
     showMessage("message-create", "plase enter some text");
   }
 });
+
+// delete todo
+function deleteTodo(id) {
+  const deleteTodos = result.filter((item, i) => {
+    return i !== id;
+  });
+  result = deleteTodos;
+  appendToLocalStorage();
+  appendToDom();
+}
+
+// set complated
+function setComplated(id) {
+  const complateTodos = result.map((item, i) => {
+    if (id == i) {
+      return { ...item, isComplate: item.isComplate == true ? false : true };
+    } else {
+      return { ...item };
+    }
+  });
+
+  result = complateTodos;
+
+  appendToLocalStorage();
+  appendToDom();
+}
+
+// edit form
+
+formEdit.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const elInputVal = formEdit["input-edit"].value.trim();
+  formEdit.reset();
+  if (elInputVal.length) {
+    result.splice(editIdItem, 1, {
+      name: elInputVal,
+      time: `${getTime()}`,
+      isComplate: false,
+    });
+    appendToLocalStorage();
+    appendToDom();
+    close();
+  } else {
+    showMessage("message-edit", "plase enter some text");
+  }
+});
+
+// edit todo
+function editTodo(id) {
+  open();
+  editIdItem = id;
+}
+
+overlay.addEventListener("click", close);
+closeEl.addEventListener("click", close);
+
+document.addEventListener("keydown", (evt) => {
+  if(evt.which == 27) {
+    close()
+  }
+
+})
+
+function open() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function close() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
